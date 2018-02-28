bcb7d9c50c9e0bd7035b766135fca65cef347a14
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-02-28 19:32:09 +0100

    Do gita

--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -10,6 +10,22 @@ namespace Shapes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Polymorphism *****\n");
+            Hexagon hex = new Hexagon("Beth");
+            hex.Draw();
+
+            Circle cir = new Circle("Cindy");
+            // Calls base class implementation!
+            cir.Draw();
+            Console.ReadLine();
+
+            Shape[] myShapes = { new Hexagon(), new Circle(), new Hexagon("Mick"), new Circle("Beth"), new Hexagon("Linda")};
+
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Shapes/Shape.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    abstract class Shape
+    {
+        public Shape(string name = "NoName")
+        {
+            PetName = name;
+        }
+
+        public string PetName { get; set; }
+        
+        //method
+        public abstract void Draw();
+    }
+
+
+    // different shape classes
+    class Circle : Shape
+    {
+        public Circle() { }
+
+        public Circle(string name) : base(name)
+        {
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Circle", PetName);
+        }
+    }
+
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Hexagon", PetName);
+        }
+    }
+}
